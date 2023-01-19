bbd93f7f40fdc464ffc922f79af4f223c9d537bd
watanabe <nekoman@mac.com>
2023-01-19 11:54:54 +0900

    Renesas RX72N/BSP 対応として変更・追加・削除

--- asp/target/rx72n_gcc/target_config.h
+++ asp/target/rx72n_gcc/target_config.h
@@ -2,10 +2,13 @@
  *  TOPPERS/ASP Kernel
  *      Toyohashi Open Platform for Embedded Real-Time Systems/
  *      Advanced Standard Profile Kernel
- * 
+ *
+ *  Copyright (C) 2000-2003 by Embedded and Real-Time Systems Laboratory
+ *                              Toyohashi Univ. of Technology, JAPAN
+ *  Copyright (C) 2005-2007 by Embedded and Real-Time Systems Laboratory
+ *              Graduate School of Information Science, Nagoya Univ., JAPAN
  *  Copyright (C) 2008-2010 by Witz Corporation, JAPAN
- *  Copyright (C) 2015- by Hisashi Hata, JAPAN
- * 
+ *
  *  上記著作権者は，以下の(1)〜(4)の条件を満たす場合に限り，本ソフトウェ
  *  ア（本ソフトウェアを改変したものを含む．以下同じ）を使用・複製・改
  *  変・再配布（以下，利用と呼ぶ）することを無償で許諾する．
@@ -28,65 +31,104 @@
  *      また，本ソフトウェアのユーザまたはエンドユーザからのいかなる理
  *      由に基づく請求からも，上記著作権者およびTOPPERSプロジェクトを
  *      免責すること．
- * 
+ *
  *  本ソフトウェアは，無保証で提供されているものである．上記著作権者お
  *  よびTOPPERSプロジェクトは，本ソフトウェアに関して，特定の使用目的
  *  に対する適合性も含めて，いかなる保証も行わない．また，本ソフトウェ
  *  アの利用により直接的または間接的に生じたいかなる損害に関しても，そ
  *  の責任を負わない．
- * 
- */
-
-/*
- *		kernel.hのプロセッサ依存部（RX用）
  *
- *  このインクルードファイルは，target_kernel.h（または，そこからインク
- *  ルードされるファイル）のみからインクルードされる．他のファイルから
- *  直接インクルードしてはならない．
  */
 
-#ifndef TOPPERS_PRC_KERNEL_H
-#define TOPPERS_PRC_KERNEL_H
-
 /*
- *  サポートする機能の定義
- */
-#define TOPPERS_TARGET_SUPPORT_DIS_INT		/* dis_intをサポートする */
-#define TOPPERS_TARGET_SUPPORT_ENA_INT		/* ena_intをサポートする */
-
-
-/*
- *  カーネル管理の割込み優先度の範囲
+ *		ターゲット依存モジュール（RX72N用）
  *
- *  TMIN_INTPRIの定義を変更することで，どのレベルよりも高い割込み優先度
- *  を持つものをカーネル管理外の割込みとするかを変更できる．
+ *  カーネルのターゲット依存部のインクルードファイル．kernel_impl.hのター
+ *  ゲット依存部の位置付けとなる．
+ */
+
+#ifndef TOPPERS_TARGET_CONFIG_H
+#define TOPPERS_TARGET_CONFIG_H
+
+/*
+ *  ターゲットシステムのハードウェア資源の定義
+ */
+#include "target_board.h"
+
+
+/*
+ *  デフォルトの非タスクコンテキスト用のスタック領域の定義
  *
- *  TMIN_INTPRIに設定できる値は，-15〜-1の範囲である．例えばTMIN_INTPRI
- *  を-14に設定すると，NMIに加えてレベル15の割込みがカーネル管理外となる．
- *  TMIN_INTPRIを-7に設定すると，NMI以外にカーネル管理外の割込みを
- *  設けないことになる．
+ *  スタックサイズを変更する場合は, sectrx.incの_DEFAULT_ISTACK
+ *  ラベルの位置にある.blkb命令のサイズと, 以下DEFAULT_ISTKSZの
+ *  サイズを合わせて同じ値に変更すること.
  */
-#ifndef TMIN_INTPRI
-#define TMIN_INTPRI		( -15 )		/* 割込み優先度の最小値（最高値）*/
-#endif /* TMIN_INTPRI */
-#define TMAX_INTPRI		( -1 )		/* 割込み優先度の最大値（最低値） */
+
+#ifndef TOPPERS_MACRO_ONLY
+extern uint32_t	DEFAULT_ISTACK[];
+#endif
+
+#define DEFAULT_ISTKSZ		(0x1000U) 
+#define DEFAULT_ISTK		( (void *)&DEFAULT_ISTACK[0] )
 
 
 /*
- *  割込み属性の定義
+ *  ターゲット依存の文字出力に使用するポートの定義
  */
-#define	TA_POSEDGE		TA_EDGE			/* ポジティブエッジトリガ */
-#define	TA_NEGEDGE		UINT_C( 0x04 )	/* ネガティブエッジトリガ */
-#define	TA_BOTHEDGE		UINT_C( 0x08 )	/* 両エッジトリガ */
+#define TARGET_PUTC_PORTID	( 1 )
 
-#define	TA_LOWLEVEL		UINT_C( 0x10 )	/* Lレベル */
+
+#ifndef TOPPERS_MACRO_ONLY
+
+/*
+ *  ターゲットシステム依存の初期化
+ */
+extern void	target_initialize( void );
+
+/*
+ *  ターゲットシステムの終了
+ *
+ *  システムを終了する時に使う．
+ */
+extern void	target_exit( void )	NoReturn;
+
+#endif /* TOPPERS_MACRO_ONLY */
 
 
 /*
- *  デフォルトの割込み/例外ハンドラの有無
+ *  プロセッサ依存モジュール（RX72n用）
  */
-/*#define	OMIT_DEFAULT_INT_HANDLER*/
-/*#define	OMIT_DEFAULT_EXC_HANDLER*/
+#include "rx72n_config.h"
 
 
-#endif /* TOPPERS_PRC_KERNEL_H */
+/*
+ *  トレースログに関する設定
+ */
+#define TCNT_TRACE_BUFFER	( 32 )
+
+/*
+ *  トレースログ出力する箇所の選択
+ *    ログ出力する箇所を1で定義
+ *    ログ出力しない箇所を0で定義
+ */
+#ifdef TOPPERS_ENABLE_TRACE
+#define LOG_DSP_ENTER	( 1 )
+#define LOG_DSP_LEAVE	( 1 )
+#define LOG_INH_ENTER	( 0 )
+#define LOG_INH_LEAVE	( 0 )
+#define LOG_EXC_ENTER	( 0 )
+#define LOG_EXC_LEAVE	( 0 )
+#else
+#define LOG_DSP_ENTER	( 0 )
+#define LOG_DSP_LEAVE	( 0 )
+#define LOG_INH_ENTER	( 0 )
+#define LOG_INH_LEAVE	( 0 )
+#define LOG_EXC_ENTER	( 0 )
+#define LOG_EXC_LEAVE	( 0 )
+#endif
+
+#ifdef TOPPERS_ENABLE_TRACE
+#include "logtrace/trace_config.h"
+#endif /* TOPPERS_ENABLE_TRACE */
+
+#endif /* TOPPERS_TARGET_CONFIG_H */
